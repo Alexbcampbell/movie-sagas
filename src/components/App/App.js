e69a7794cd9f533abbf550ab8f42a3e44d46a584
Alex Campbell <alexbe.campbell@gmail.com>
2020-11-14 21:26:22 -0600

    added links to home page and new movie form to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import AddMoviePage from '../../pages/AddMoviesPage/AddMoviePage';
 import DetailsPage from '../../pages/DetailsPage/DetailsPage';
 import HomePage from '../../pages/HomePage/HomePage';
@@ -9,15 +9,22 @@ class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div className="App">
-        <h1>Movies!</h1>
-        <nav></nav>
-        <Router>
+      <Router>
+        <div className="App">
+          <h1>Movies!</h1>
+          <nav>
+            <h3>
+              <Link to="/">Home Page</Link>
+            </h3>
+            <h3>
+              <Link to="/add">Add New Movie</Link>
+            </h3>
+          </nav>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/details/:id" component={DetailsPage} />
           <Route exact path="/add" component={AddMoviePage} />
-        </Router>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
