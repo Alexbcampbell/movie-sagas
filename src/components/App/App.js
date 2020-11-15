5e11c8cf0a6e80e02bb4af9924942e8961aca50e
Alex Campbell <alexbe.campbell@gmail.com>
2020-11-15 13:21:29 -0600

    created Header component, added some CSS styling to header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import AddMoviePage from '../../pages/AddMoviesPage/AddMoviePage';
 import DetailsPage from '../../pages/DetailsPage/DetailsPage';
 import HomePage from '../../pages/HomePage/HomePage';
+import Header from '../Header/Header';
 import './App.css';
 
 class App extends Component {
@@ -11,15 +12,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <h1>Movies!</h1>
-          <nav>
-            <h3>
-              <Link to="/">Home Page</Link>
-            </h3>
-            <h3>
-              <Link to="/add">Add New Movie</Link>
-            </h3>
-          </nav>
+          <Header />
           <Route exact path="/" component={HomePage} />
           <Route exact path="/details/:id" component={DetailsPage} />
           <Route exact path="/add" component={AddMoviePage} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="App-header">Movies!</h1>
+        <nav>
+          <h3>
+            <Link to="/">Home Page</Link>
+          </h3>
+          <h3>
+            <Link to="/add">Add New Movie</Link>
+          </h3>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Header;
